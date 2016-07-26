de5da439bc7aca14b9290e15e00ea8daff25e9d7
DED8IRD <ewu72829@gmail.com>
2016-07-26 12:47:49 -0700

    integrated generator (renamed to HAL.py) with dave and stanley

--- Code/Development/DAVE.py
+++ Code/Development/DAVE.py
@@ -5,11 +5,10 @@ Generates screenplay using Markov models of a genre-separated
 screenplay corpus.
 '''
 
-import zipfile, argparse, os, nltk, operator, re, sys, random
+import argparse, nltk, random, pickle
 from collections import defaultdict
-from nltk.text import ContextIndex
-from nltk.parse.stanford import StanfordParser
-from generator import sentGenerator
+from HAL import sentGenerator as gen
+from Stanley import director
 
 # Parser
 parser = argparse.ArgumentParser()
@@ -19,15 +18,35 @@ args = parser.parse_args()
 
 if __name__ == '__main__':
 
-    base_text = open(args.i, 'rU', encoding='utf-8').read()
-    base_sents = nltk.sent_tokenize(base_text)
-    gen = sentGenerator(base_text)
+    genre = args.i
+    # iterate through all the files in the genre's directory
+    scriptFormat = {}
+    filenames = [('headings', genre + '.Headings'),
+                 ('characters', genre + '.Characters'),
+                 ('parentheticals', genre + '.Parentheticals'),
+                 ('transitions', genre + '.Transitions'),
+                 ('actions', genre + '.Actions.pickle'),
+                 ('dialogue', genre + '.Dialogue.pickle')]
+
+    for (var, filename) in filenames:
+        if filename.endswith('.pickle'):
+            loadfile = pickle.load(open(filename, 'rb'))
+        else:
+            loadfile = open(filename, 'rU', encoding='utf-8').readlines()
+            for line in range(len(loadfile)):
+                loadfile[line] = loadfile[line].strip()
+        scriptFormat[var] = loadfile
+
+    headings = scriptFormat['headings']
+    characters = scriptFormat['characters']
+    parentheticals = scriptFormat['parentheticals']
+    transitions = scriptFormat['transitions']
+    actions = scriptFormat['actions']
+    dialogue = scriptFormat['dialogue']
+
+    actGen = gen(actions)
+    diaGen = gen(dialogue)
     output = open(args.o, 'w', encoding="utf-8")
-    text = gen()
-    output.write(text)
-
-    # text = nltk.Text(base_text)
-    # text.generate()
-
-
-
+    stan = director(headings, characters, parentheticals, transitions,
+                    actGen, diaGen, output)
+    stan()
